c50a8befa94d9af68ee6afc57bd080ee0f0bd151
Adriano Campos <adrianohcampos@gmail.com>
2022-08-18 15:10:30 -0300

    init

---
+++ js/jsDrawing.js
@@ -0,0 +1,80 @@
+
+var jsDrawing = [];
+
+jsDrawing.createCanvas = function (parent, width, height) {
+    var canvas = {};
+    canvas.node = document.createElement('canvas');
+    canvas.context = canvas.node.getContext('2d');
+    canvas.node.width = width || 100;
+    canvas.node.height = height || 100;
+    parent.appendChild(canvas.node);
+    return canvas;
+}
+
+jsDrawing.init = function (element, width, height, fillColor) {
+
+    var container = document.querySelector(element);
+    var canvas = jsDrawing.createCanvas(container, width, height);
+    var ctx = canvas.context;
+
+    canvas.drawColor = fillColor;
+    canvas.drawWidth = 5;
+    canvas.isDrawing = false;
+
+    ctx.start = function (e) {
+        canvas.isDrawing = true;
+        ctx.beginPath();
+        ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        e.preventDefault();
+    }
+
+    ctx.stop = function (e) {
+        if (canvas.isDrawing) {
+            ctx.stroke()
+            ctx.closePath()
+            canvas.isDrawing = false;
+        }
+        e.preventDefault();
+    }
+
+    ctx.draw = function (e) {
+        if (canvas.isDrawing) {
+            ctx.lineTo(e.clientX - canvas.node.offsetLeft, e.clientY - canvas.node.offsetTop);
+            ctx.strokeStyle = canvas.drawColor;
+            ctx.lineWidth = canvas.drawWidth;
+            ctx.lineCap = "round"
+            ctx.lineJoin = "round"
+            ctx.stroke()
+        }
+        e.preventDefault();
+    }
+
+    ctx.clearTo = function () {
+        ctx.fillStyle = "rgba(255, 255, 255, 0)";
+        ctx.fillRect(0, 0, width, height);
+    };
+
+    ctx.clearTo();
+
+    // bind mouse events
+    canvas.node.onmousemove = function (e) {
+        ctx.draw(e)
+    };
+    canvas.node.onmousedown = function (e) {
+        ctx.start(e)
+    };
+    canvas.node.onmouseup = function (e) {
+        ctx.stop(e)
+    };
+
+    // bind touch events
+    canvas.node.ontouchmove = function (e) {
+        ctx.draw(e)
+    };
+    canvas.node.ontouchdown = function (e) {
+        ctx.start(e)
+    };
+    canvas.node.ontouchup = function (e) {
+        ctx.stop(e)
+    };
+}
